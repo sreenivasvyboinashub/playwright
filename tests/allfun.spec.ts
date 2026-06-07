2b55c3321de8beb9e908643c17c0fdb48240be20
vaasuvyboina <vybhaw.vasu.vyboina@gmail.com>
2026-06-07 22:39:32 +0530

    ghj

--- tests/allfun.spec.ts
+++ tests/allfun.spec.ts
@@ -5,11 +5,12 @@ test('test', async ({ page }) => {
   await page.goto('https://ui.shadcn.com/docs/components/radix/tooltip');
   
  //const tooltipButton = page.locator("//button[text()='Hover']");
- //await tooltipButton.hover();
+ //await tooltipButton.hover();//
  await page.getByRole('button', { name: 'Hover' }).hover();
  await page.waitForTimeout(30000);
  
-  await expect(page.getByRole('tooltip')).toBeVisible();
+ await expect(page.getByRole('tooltip')).toBeVisible();
+
 //    // Wait for the tooltip to appear
 //   const tooltip = page.getByRole('tooltip');
 //   await expect(tooltip).toBeVisible();
